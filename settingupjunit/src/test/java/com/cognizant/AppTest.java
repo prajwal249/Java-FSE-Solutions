c41498abeb5007c85d85b9437cfc68941bec5712
prajwal249 <halemaniprajwal8@gmail.com>
2026-07-02 15:57:41 +0530

    setting junit5
    added settingupjunit5

---
+++ settingupjunit/src/test/java/com/cognizant/AppTest.java
@@ -0,0 +1,13 @@
+package com.cognizant;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
